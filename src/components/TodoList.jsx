82dd0334fa5e3a323f69dd77d9a36394709f1c0f
Agustin Garcia <agusstingarcia144@gmail.com>
2023-01-23 17:20:22 -0300

    first commit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import CheckIcon from '@mui/icons-material/Check';
+import ClearIcon from '@mui/icons-material/Clear';
+import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
+
+const TodoList = ({ todos, setTodos, setEditTodo }) => {
+
+    const handleComplete = (todo) => {
+        setTodos(todos.map((item) => {
+            if (item.id === todo.id) {
+                return { ...item, completed: !item.completed};
+            }
+            return item;
+        })
+        );
+    };
+
+    const handleEdit = ({ id }) => {
+        const findTodo = todos.find((todo) => todo.id === id);
+        setEditTodo(findTodo);
+    };
+
+    const handleDelete = ({ id }) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
+
+  return (
+    <div>
+        {todos.map((todo) => (
+            <li className="list-item" key={todo.id}>
+                <input type="text" value={todo.title} className={`list ${todo.completed ? "complete" : ""}`} onChange={(event) => event.preventDefault()}  />
+                <div>
+                    
+                    <button className="button-complete task-button" onClick={() => handleComplete(todo)}>
+                        <CheckIcon></CheckIcon>
+                    </button>
+                     <button className="button-edit task-button" onClick={() => handleEdit(todo)}>
+                        <ModeEditOutlineOutlinedIcon></ModeEditOutlineOutlinedIcon>
+                    </button> 
+                    <button className="button-delete task-button" onClick={() => handleDelete(todo)}>
+                        <ClearIcon></ClearIcon>
+                    </button>
+                </div>
+            </li>
+        ))}
+    </div>
+  )
+}
+
+export default TodoList
